a671d63525979b6106743f5b91c127fd7cacf49d
Conor Walsh <conorwalsh0703@gmail.com>
2020-04-04 14:17:42 +0100

    setup controllers

---
+++ app.js
@@ -0,0 +1,63 @@
+// Storage Controller
+
+
+// Item Controller
+const ItemCtrl = (function () {
+    // Item Constructor
+    const Item = function (id, name, calories) {
+        this.id = id
+        this.name = name
+        this.calories = calories
+    }
+
+    // Data Structure / State
+    const data = {
+        items: [{
+                id: 0,
+                name: 'Steak Dinner',
+                calories: 1200
+            },
+            {
+                id: 1,
+                name: 'Cookie',
+                calories: 250
+            },
+            {
+                id: 2,
+                name: 'Eggs',
+                calories: 300
+            },
+        ],
+        currentItem: null,
+        totalCalories: 0
+    }
+    // Public Methods
+    return {
+        logData: function () {
+            return data
+        }
+    }
+})()
+
+// UI Controller
+const UICtrl = (function () {
+
+    // Public Methods
+    return {
+
+    }
+})()
+
+// App Controller
+const App = (function (ItemCtrl, UICtrl) {
+
+    // Public Methods
+    return {
+        init: function () {
+
+        }
+    }
+})(ItemCtrl, UICtrl)
+
+// Initialize App
+App.init()
